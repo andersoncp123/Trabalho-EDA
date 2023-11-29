50b68deed2d0c4671184822712de367858d23530
Vitório <vitoriofernandes593@gmail.com>
2023-11-29 16:24:10 -0300

    Criando os métodos necessários

--- Segunda_Questao.c
+++ Segunda_Questao.c
@@ -5,13 +5,48 @@ typedef struct no{
 } tPilhaSimples;
 
 //testar se a pilha está vazia
+int testePilhaVazia(tPilhaSimples *pilha) {
+    if (pilha == NULL || pilha->proximo == NULL) {// Verifica se a pilha está vazia ou o próximo elemento é NULL;
+        return 0;
+    } else {
+        return 1;
+    }
+}
 
 //criar uma pilha unitária (se estiver vazia)
+tPilhaSimples *criarPilha(int dadoNovo) {
+    tPilhaSimples *novoNo = (tPilhaSimples *)malloc(sizeof(tPilhaSimples));// Fazendo a alocaçao dinâmica 
+    if (novoNo != NULL) {
+        novoNo->dado = dadoNovo;
+        novoNo->proximo = NULL;
+    }
+    return novoNo;
+}
 
 //empilhar números
-
+tPilhaSimples *empilhar(int dadoNovo,tPilhaSimples *topoPilha){
+    tPilhaSimples *novoTopoPilha;
+    novoTopoPilha = malloc(sizeof(tPilhaSimples));
+    if (novoTopoPilha == NULL){
+        return topoPilha; // Pilha Vázia 
+    }else{
+        novoTopoPilha ->dado = dadoNovo;
+        novoTopoPilha ->proximo = topoPilha;
+        return novoTopoPilha;
+    }
+}
 //desempilhar números (caso não esteja vazia)
-
+tPilhaSimples *empilhar(tPilhaSimples *topoPilha){
+    tPilhaSimples *novoTopoPilha;
+    novoTopoPilha = malloc(sizeof(tPilhaSimples));
+    if (novoTopoPilha == NULL){
+        return topoPilha; // Pilha Vázia 
+    }else{
+        novoTopoPilha = topoPilha -> proximo;
+        free(topoPilha);
+        return novoTopoPilha;
+    }
+}
 //mostrar os elementos em forma de pilha
 
 //opção para finalizar o programa, limpar a pilha e mencionar a quantidade de nós liberados
